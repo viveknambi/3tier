823fd081c56b426eff8cdc29e6d058ae23ac22f0
Rob Kinyon <rob.kinyon@gmail.com>
2019-03-25 20:16:42 -0400

    Add --no-include-email to the login command

--- devops/bin/travis_deploy.sh
+++ devops/bin/travis_deploy.sh
@@ -5,7 +5,7 @@ if ! command -v aws >/dev/null 2>&1; then
   export PATH=$PATH:$HOME/.local/bin # put aws in the path
 fi
 
-eval $(aws ecr get-login --region us-east-1)
+eval $(aws ecr get-login --region us-east-1 --no-include-email)
 
 docker tag 3tier_api:latest ${REGISTRY}/three_tier_api
 docker push ${REGISTRY}/three_tier_api
