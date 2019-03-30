d9e35def479f137464cf12d54df9e24dda0b52a2
Rob Kinyon <rob.kinyon@gmail.com>
2019-03-30 16:29:58 -0400

    Test out a new way of deploying in travis

--- web/routes/index.js
+++ web/routes/index.js
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
       } else {
         res.render('index', {
           title: '3tier App' ,
-          request_uuid: body.request_uuid + " x3",
+          request_uuid: body.request_uuid + " x4",
           time: body.time
         });
       }
